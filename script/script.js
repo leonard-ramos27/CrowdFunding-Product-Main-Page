750d46f7d91ed24ea1ea60008615f2408c648ace
leonard-ramos27 <117474533+leonard-ramos27@users.noreply.github.com>
2023-01-18 02:36:33 +0800

    Fixed Modal Screens
    Fixed issue with the Modal screens and rearranged JavaScript code into three sections: Selectors, Event Listeners, Functions.

--- script/script.js
+++ script/script.js
@@ -1,26 +1,59 @@
-
-//Burger Menu switch
+//SELECTORS
 
 const nav = document.getElementById('nav-bar');
 const header = document.getElementById('header');
 const burgericon = document.getElementById('burger-icon');
 const burgerclose = document.getElementById('burger-close');
 
-burgericon.addEventListener('click', () => {
+const btn_back_project = document.getElementById('btn-back-project');
+const modal_back_project = document.getElementById('modal-back-project');
+const btn_close_modal = document.getElementById('btn-close-modal');
+const main_window = document.getElementById('main-window');
+
+const btn_bookmark = document.getElementById('btn-bookmark');
+
+const radio_noreward = document.getElementById('radio-noreward');
+const radio_bamboo = document.getElementById('radio-bamboo');
+const radio_blackEdition = document.getElementById('radio-black-edition');
+const div_pledge_noreward = document.getElementById('div-pledge-noreward');
+const div_pledge_bamboo = document.getElementById('div-pledge-bamboo');
+const div_pledge_blackedition = document.getElementById('div-pledge-blackedition');
+
+const btn_select_blackEdition = document.getElementById('btn-select-black-edition');
+const btn_select_bamboo = document.getElementById('btn-select-bamboo');
+
+const btn_continue_noreward = document.getElementById('btn-continue-noreward');
+const btn_continue_bamboo = document.getElementById('btn-continue-bamboo');
+const btn_continue_blackedition = document.getElementById('btn-continue-blackedition');
+const modal_completed = document.getElementById('modal-completed');
+const btn_close_completed = document.getElementById('btn-close-completed');
+const txt_pledge_noreward = document.getElementById('txt-pledge-noreward');
+const txt_pledge_bamboo = document.getElementById('txt-pledge-bamboo');
+const txt_pledge_blackedition = document.getElementById('txt-pledge-blackedition');
+
+
+
+//EVENT LISTENERS
+
+
+burgericon.addEventListener('click', () => {    
+    //Open Burger Menu
     nav.classList.add('nav-bar-show');
     nav.classList.add('nav-bar-transition');
     burgericon.classList.add('hidden');
     burgerclose.classList.remove('hidden');
 })
 
-burgerclose.addEventListener('click', () => {
+burgerclose.addEventListener('click', () => {   
+    //Close Burger Menu 
     nav.classList.remove('nav-bar-show');
     nav.classList.add('nav-bar-transition');
     burgericon.classList.remove('hidden');
     burgerclose.classList.add('hidden');
 })
 
-window.onresize = () => {
+window.onresize = () => {       
+    //Resetting Burger Menu when window size is changed
     let viewPortWidth = window.innerWidth;
     if(viewPortWidth > 848){
         nav.classList.remove('nav-bar-show');
@@ -30,68 +63,34 @@ window.onresize = () => {
     }
 }
 
-// Back this Project Modal Screen Toggle 
-//Including Event Listener for Button-Back this Project and Button-Close Modal
+btn_bookmark.addEventListener("click", toggleBookmark); 
 
-const btn_back_project = document.getElementById('btn-back-project');
-const modal_back_project = document.getElementById('modal-back-project');
-const btn_close_modal = document.getElementById('btn-close-modal');
-const main_window = document.getElementById('main-window');
-
-function toggleModal(){
-    modal_back_project.classList.toggle("show-modal");
-    main_window.classList.toggle("main-hide-overflow");
-}
-
-function windowOnClick(event){
-    if (event.target === modal_back_project) {
-        console.log('closing modal')
-        toggleModal();
-        hidePledgeSections();
-    }else if(event.target === modal_completed){
-        console.log('closing modal 2')
-        toggleModalCompleted();
-    }
-}
-
-btn_back_project.addEventListener("click", () =>{
+btn_back_project.addEventListener("click", () => {   
+    //Open Back this Project Modal
     toggleModal();
 });
 
-btn_close_modal.addEventListener("click", () =>{
+btn_close_modal.addEventListener("click", () => {    
+    //Close Back this Project Modal
     toggleModal();
     hidePledgeSections();
 });
 
-window.addEventListener("click", windowOnClick);
+window.addEventListener("click", windowOnClick);  
 
-// Bookmark switch
+btn_select_bamboo.addEventListener('click', () => {
+    //Selects Bamboo Stand Reward
+    radio_bamboo.checked = true;
+    div_pledge_bamboo.classList.add('show-pledge-details');
+    toggleModal();
+})
 
-const btn_bookmark = document.getElementById('btn-bookmark');
-
-function toggleBookmark(){
-    btn_bookmark.classList.toggle("bookmark-selected");
-}
-
-btn_bookmark.addEventListener("click", toggleBookmark);
-
-//Show Enter Pledge Section
-
-const radio_noreward = document.getElementById('radio-noreward');
-const radio_bamboo = document.getElementById('radio-bamboo');
-const radio_blackEdition = document.getElementById('radio-black-edition');
-const div_pledge_noreward = document.getElementById('div-pledge-noreward');
-const div_pledge_bamboo = document.getElementById('div-pledge-bamboo');
-const div_pledge_blackedition = document.getElementById('div-pledge-blackedition');
-
-function hidePledgeSections(){
-    div_pledge_noreward.classList.remove('show-pledge-details');
-    div_pledge_bamboo.classList.remove('show-pledge-details');
-    div_pledge_blackedition.classList.remove('show-pledge-details');
-    radio_noreward.checked = false;
-    radio_bamboo.checked = false;
-    radio_blackEdition.checked = false;
-}
+btn_select_blackEdition.addEventListener('click', () => {
+    //Selects Black Edition Stand Reward
+    radio_blackEdition.checked = true;
+    div_pledge_blackedition.classList.add('show-pledge-details');
+    toggleModal();
+})
 
 radio_noreward.addEventListener('change', () => {
     hidePledgeSections();
@@ -114,41 +113,74 @@ radio_blackEdition.addEventListener('change', () => {
     }
 })
 
-// Select Reward Buttons Event Listeners
+btn_continue_noreward.addEventListener('click', () => {
+    event.preventDefault();
+    const pledgeAmount = parseFloat(txt_pledge_noreward.value);
+    if(pledgeAmount >= 1){
+        toggleModalCompleted();
+    }
+});
 
-const btn_select_blackEdition = document.getElementById('btn-select-black-edition');
-const btn_select_bamboo = document.getElementById('btn-select-bamboo');
+btn_continue_bamboo.addEventListener('click', () => {
+    event.preventDefault();
+    const pledgeAmount = parseFloat(txt_pledge_bamboo.value);
+    if(pledgeAmount >= 25){
+        toggleModalCompleted();
+    }
+});
 
-btn_select_blackEdition.addEventListener('click', () => {
-    radio_blackEdition.checked = true;
-    div_pledge_blackedition.classList.add('show-pledge-details');
-    toggleModal();
-})
-
-btn_select_bamboo.addEventListener('click', () => {
-    radio_bamboo.checked = true;
-    div_pledge_bamboo.classList.add('show-pledge-details');
-    toggleModal();
-})
-
-//Modal Completed Screen Toggle
-
-const btn_continue = document.getElementById('btn-continue');
-const modal_completed = document.getElementById('modal-completed');
-const btn_close_completed = document.getElementById('btn-close-completed');
-
-function toggleModalCompleted(){
-    modal_completed.classList.toggle("show-modal");
-    main_window.classList.toggle("main-hide-overflow");
-}
-
-btn_continue.addEventListener('click', ()=>{
-    console.log('button continue pressed');
-    toggleModal();
-    hidePledgeSections();
-    toggleModalCompleted();
-})
+btn_continue_blackedition.addEventListener('click', () => {
+    event.preventDefault();
+    const pledgeAmount = parseFloat(txt_pledge_blackedition.value);
+    if(pledgeAmount >= 75){
+        toggleModalCompleted();
+    }
+});
 
 btn_close_completed.addEventListener('click', ()=>{
     toggleModalCompleted();
-})
+    toggleModal();
+    hidePledgeSections();
+});
+
+
+
+//FUNCTIONS
+
+function toggleModal(){
+    //Opens Back this Project Modal 
+    modal_back_project.classList.toggle("show-modal");
+    main_window.classList.toggle("main-hide-overflow");
+}
+
+function windowOnClick(event){
+    //Closes Modal when clicking the gray background screen
+    if (event.target === modal_back_project) {
+        console.log('closing modal')
+        toggleModal();
+        hidePledgeSections();
+    }else if(event.target === modal_completed){
+        console.log('closing modal 2')
+        toggleModalCompleted();
+    }
+}
+
+function toggleBookmark(){
+    //Turns on and off the Bookmark
+    btn_bookmark.classList.toggle("bookmark-selected");
+}
+
+function hidePledgeSections(){
+    //Hides all Enter Pledge Amount sections
+    div_pledge_noreward.classList.remove('show-pledge-details');
+    div_pledge_bamboo.classList.remove('show-pledge-details');
+    div_pledge_blackedition.classList.remove('show-pledge-details');
+    //Unchecks all radio buttons
+    radio_noreward.checked = false;
+    radio_bamboo.checked = false;
+    radio_blackEdition.checked = false;
+}
+
+function toggleModalCompleted(){
+    modal_completed.classList.toggle("show-modal");
+}
