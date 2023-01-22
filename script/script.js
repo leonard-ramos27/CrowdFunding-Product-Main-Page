d5c42e94b167e7589622c64e48952408e57654dc
leonard-ramos27 <117474533+leonard-ramos27@users.noreply.github.com>
2023-01-22 15:12:00 +0800

    Updated Javascript
    Updated code for adding pledge and updating stats on main page.

--- script/script.js
+++ script/script.js
@@ -4,24 +4,19 @@ const nav = document.getElementById('nav-bar');
 const header = document.getElementById('header');
 const burgericon = document.getElementById('burger-icon');
 const burgerclose = document.getElementById('burger-close');
-
 const btn_back_project = document.getElementById('btn-back-project');
 const modal_back_project = document.getElementById('modal-back-project');
 const btn_close_modal = document.getElementById('btn-close-modal');
 const main_window = document.getElementById('main-window');
-
 const btn_bookmark = document.getElementById('btn-bookmark');
-
 const radio_noreward = document.getElementById('radio-noreward');
 const radio_bamboo = document.getElementById('radio-bamboo');
 const radio_blackEdition = document.getElementById('radio-black-edition');
 const div_pledge_noreward = document.getElementById('div-pledge-noreward');
 const div_pledge_bamboo = document.getElementById('div-pledge-bamboo');
 const div_pledge_blackedition = document.getElementById('div-pledge-blackedition');
-
 const btn_select_blackEdition = document.getElementById('btn-select-black-edition');
 const btn_select_bamboo = document.getElementById('btn-select-bamboo');
-
 const btn_continue_noreward = document.getElementById('btn-continue-noreward');
 const btn_continue_bamboo = document.getElementById('btn-continue-bamboo');
 const btn_continue_blackedition = document.getElementById('btn-continue-blackedition');
@@ -33,13 +28,17 @@ const txt_pledge_blackedition = document.getElementById('txt-pledge-blackedition
 const div_input_noreward = document.getElementById('div-input-noreward');
 const div_input_bamboo = document.getElementById('div-input-bamboo');
 const div_input_blackedition = document.getElementById('div-input-blackedition');
+const div_total_pledge = document.getElementById('pledge-total-amount');
+const div_total_backers = document.getElementById('pledge-total-backers');
+const stats_progress_bar = document.querySelector('.stat-progress-bar');
+const span_bamboo_count = document.querySelectorAll('.bamboo-count');
+const span_black_edition_count = document.querySelectorAll('.black-edition-count');
 let pledgeStats = {
     totalPledge: 89914,
-    totalBackers: 5007
+    totalBackers: 5007,
+    bambooItemCount: 101,
+    blackEditionItemCount: 64
 };
-
-
-
 
 //EVENT LISTENERS
 
@@ -49,69 +48,19 @@ burgericon.addEventListener('click', openMobileMenu)
 burgerclose.addEventListener('click', closeMobileMenu)
 btn_bookmark.addEventListener("click", toggleBookmark)
 btn_back_project.addEventListener("click", toggleModal)
-
+btn_select_bamboo.addEventListener('click', btnSelectRewardClicked)
+btn_select_blackEdition.addEventListener('click', btnSelectRewardClicked)
+radio_noreward.addEventListener('click', radioBtnChecked)
+radio_bamboo.addEventListener('change', radioBtnChecked)
+radio_blackEdition.addEventListener('change', radioBtnChecked)
+btn_continue_noreward.addEventListener('click', btnContinueClicked)
+btn_continue_bamboo.addEventListener('click', btnContinueClicked)
+btn_continue_blackedition.addEventListener('click', btnContinueClicked);
 btn_close_modal.addEventListener("click", () => {    
     toggleModal()
     hidePledgeSections()
     clearRadioButtons()
 })
-
-btn_select_bamboo.addEventListener('click', btnSelectRewardClicked)
-/*() => {
-    selectBambooReward()
-    toggleModal()
-}*/
-
-btn_select_blackEdition.addEventListener('click', btnSelectRewardClicked)
-/*() => {
-    selectBlackEditionReward()
-    toggleModal()
-}*/
-
-radio_noreward.addEventListener('click', radioBtnChecked)
-radio_bamboo.addEventListener('change', radioBtnChecked)
-radio_blackEdition.addEventListener('change', radioBtnChecked)
-
-btn_continue_noreward.addEventListener('click', (event)=>{
-    event.preventDefault();
-    btnContinueClicked('noreward', parseFloat(txt_pledge_noreward.value))
-})
-
-/*(event) => {
-    event.preventDefault();
-    const pledgeAmount = parseFloat(txt_pledge_noreward.value);
-    if(pledgeAmount >= 1){
-        toggleModalCompleted();
-        div_input_noreward.classList.remove('invalid-value')
-    }else{
-        div_input_noreward.classList.add('invalid-value')
-    }
-});*/
-
-btn_continue_bamboo.addEventListener('click', (event) => {
-    event.preventDefault();
-    btnContinueClicked('bamboo', parseFloat(txt_pledge_bamboo.value))
-    /*const pledgeAmount = parseFloat(txt_pledge_bamboo.value);
-    if(pledgeAmount >= 25){
-        toggleModalCompleted();
-        div_input_bamboo.classList.remove('invalid-value')
-    }else{
-        div_input_bamboo.classList.add('invalid-value')
-    }*/
-});
-
-btn_continue_blackedition.addEventListener('click', (event) => {
-    event.preventDefault();
-    btnContinueClicked('blackedition', parseFloat(txt_pledge_blackedition.value))
-    /*const pledgeAmount = parseFloat(txt_pledge_blackedition.value);
-    if(pledgeAmount >= 75){
-        toggleModalCompleted();
-        div_input_blackedition.classList.remove('invalid-value')
-    }else{
-        div_input_blackedition.classList.add('invalid-value')
-    }*/
-});
-
 btn_close_completed.addEventListener('click', ()=>{
     toggleModalCompleted();
     toggleModal();
@@ -122,7 +71,6 @@ btn_close_completed.addEventListener('click', ()=>{
 
 
 //FUNCTIONS
-
 
 function openMobileMenu(){
     //Open Burger Menu
@@ -151,10 +99,25 @@ function resetMobileMenu(){
     }
 }
 
+function toggleBookmark(){
+    //Turns on and off the Bookmark
+    btn_bookmark.classList.toggle("bookmark-selected");
+    //Changes Text to Bookmarked if turned on
+    if(btn_bookmark.classList.contains("bookmark-selected")){
+        btn_bookmark.children[1].innerHTML = "Bookmarked"
+    }else{
+        btn_bookmark.children[1].innerHTML = "Bookmark"
+    }
+}
+
 function toggleModal(){
-    //Opens Back this Project Modal 
+    //Opens Modal of Back this project
     modal_back_project.classList.toggle("show-modal");
     main_window.classList.toggle("main-hide-overflow");
+}
+
+function toggleModalCompleted(){
+    modal_completed.classList.toggle("show-modal");
 }
 
 function windowOnClick(event){
@@ -165,16 +128,6 @@ function windowOnClick(event){
         clearRadioButtons();
     }else if(event.target === modal_completed){
         toggleModalCompleted();
-    }
-}
-
-function toggleBookmark(){
-    //Turns on and off the Bookmark
-    btn_bookmark.classList.toggle("bookmark-selected");
-    if(btn_bookmark.classList.contains("bookmark-selected")){
-        btn_bookmark.children[1].innerHTML = "Bookmarked"
-    }else{
-        btn_bookmark.children[1].innerHTML = "Bookmark"
     }
 }
 
@@ -204,11 +157,16 @@ function clearRadioButtons(){
     radio_blackEdition.checked = false;
 }
 
-function toggleModalCompleted(){
-    modal_completed.classList.toggle("show-modal");
+function clearErrorInvalidValue(){
+    //Removes the error invalid value on all input boxes
+    div_input_noreward.classList.remove('invalid-value')
+    div_input_bamboo.classList.remove('invalid-value')
+    div_input_blackedition.classList.remove('invalid-value')
 }
 
-function selectBambooReward(){
+
+
+/*function selectBambooReward(){
     hidePledgeSections();
     if (radio_bamboo.checked = true ){
         div_pledge_bamboo.classList.add('show-pledge-details');
@@ -230,7 +188,7 @@ function selectNoReward(){
         div_pledge_noreward.classList.add('show-pledge-details');
         radio_noreward.closest('.modal-product-container').classList.add('green-border')
     }
-}
+}*/
 
 function btnSelectRewardClicked(event){
     //Checks the type of reward selected
@@ -245,7 +203,6 @@ function btnSelectRewardClicked(event){
 
 function radioBtnChecked(){
     hidePledgeSections();
-    console.log(radio_noreward.checked, radio_bamboo.checked, radio_blackEdition.checked)
     if (radio_noreward.checked === true ){
         div_pledge_noreward.classList.add('show-pledge-details');
         radio_noreward.closest('.modal-product-container').classList.add('green-border')
@@ -258,7 +215,7 @@ function radioBtnChecked(){
     }
 }
 
-function btnContinueClicked(pledgeType, pledgeAmount){
+/*function btnContinueClicked(pledgeType, pledgeAmount){
     console.log(pledgeType, pledgeAmount)
     switch(pledgeType){
         case "noreward":
@@ -292,12 +249,89 @@ function btnContinueClicked(pledgeType, pledgeAmount){
                 break;
             }
     }
+}*/
+
+function btnContinueClicked(event){
+    event.preventDefault()
+    //Check if user selected No Reward
+    if(event.target === btn_continue_noreward){
+        let pledgeAmount = parseFloat(txt_pledge_noreward.value)
+        //Check if the amount entered is more than 1
+        if(pledgeAmount >= 1){
+            //Removes all error messages for invalid values
+            clearErrorInvalidValue()
+            //Add amount to total Pledge
+            addPledge(pledgeAmount)
+            //Show Modal Completed
+            toggleModalCompleted()
+            //Update stats in Main Page
+            updateStats()
+        }else{
+            //Shows message that value is invalid
+            div_input_noreward.classList.add('invalid-value')
+        }
+    }
+    //Check if user selected the Bamboo Stand Reward
+    else if(event.target === btn_continue_bamboo){
+        let pledgeAmount = parseFloat(txt_pledge_bamboo.value)
+        if(pledgeAmount >= 25){
+            clearErrorInvalidValue()
+            addPledge(pledgeAmount)
+            //Decrease item left for Bamboo Stand
+            decrementBamboo()
+            toggleModalCompleted()
+            updateStats()
+        }else{
+            div_input_bamboo.classList.add('invalid-value')
+        }
+    }
+    //Check if user selected Black Edition Stand Reward
+    else if(event.target === btn_continue_blackedition){
+        let pledgeAmount = parseFloat(txt_pledge_blackedition.value)
+        if(pledgeAmount >= 75){
+            clearErrorInvalidValue()
+            addPledge(pledgeAmount)
+            //Decrease item left for Black Edition Stand
+            decrementBlackEdition()
+            toggleModalCompleted()
+            updateStats()
+        }else{
+            div_input_blackedition.classList.add('invalid-value')
+        }
+    }
+}
+
+function decrementBamboo(){
+    pledgeStats.bambooItemCount--;
+}
+
+function decrementBlackEdition(){
+    pledgeStats.blackEditionItemCount--;
 }
 
 function addPledge(pledgeAmount){
     pledgeStats.totalPledge += pledgeAmount;
     pledgeStats.totalBackers++;
-    console.log(pledgeStats.totalPledge, pledgeStats.totalBackers)
 }
 
+function updateStats(){
+    //Formats USD currency for Total Pledge Amount
+    const f = new Intl.NumberFormat("en-us", {
+        style: "currency", 
+        currency: "USD", 
+        minimumFractionDigits: 0
+    })
+    //Updates Total Pledge Amount, Total Backers and Progress Bar in Main Page
+    div_total_pledge.innerText = f.format(pledgeStats.totalPledge)
+    div_total_backers.innerText = pledgeStats.totalBackers
+    stats_progress_bar.value = pledgeStats.totalPledge
+    //Updates total item left for Bamboo Stand in Main Page and Modal
+    span_bamboo_count.forEach(span_item_count => {
+        span_item_count.innerText = pledgeStats.bambooItemCount
+    });
+    //Updates total item left for Bamboo Stand in Main Page and Modal
+    span_black_edition_count.forEach(span_item_count => {
+        span_item_count.innerText = pledgeStats.blackEditionItemCount
+    });
+}
 
